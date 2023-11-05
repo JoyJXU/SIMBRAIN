4bb0dfc4416b19e75374c21a1f6d9e0148500e11
einseinseins <yi_zheng_0300@163.com>
2023-11-05 22:01:45 +0800

    Retention_Loss_version4

--- bindsnet/network/nodes.py
+++ bindsnet/network/nodes.py
@@ -206,7 +206,12 @@ class Nodes(torch.nn.Module):
                     # G(t) = G(0) * e^(- t*tau)^beta                      
                     self.x *=  torch.exp(torch.tensor(-(1/2 * delta_t * retention_loss_tau) ** retention_loss_beta))
                     self.x = torch.clamp(self.x, min = G_on, max = G_off)
-                    # tau = 0.12478 , beta = 1.066  or  tau = 0.1245 , beta = 1.073
+                    
+                    self.mem_x = G_off * self.mem_x + G_on * (1 - self.mem_x)
+                    self.mem_x *= torch.exp(torch.tensor(-(1/2 * delta_t * retention_loss_tau) ** retention_loss_beta))
+                    self.mem_x = torch.clamp(self.mem_x, min = G_on, max = G_off)
+                    self.mem_x = (self.mem_x - G_on) * trans_ratio
+                    # tau = 0.012478 , beta = 1.066  or  tau = 0.01245 , beta = 1.073
                     
     
                 if self.retention_loss == 2:
@@ -214,8 +219,13 @@ class Nodes(torch.nn.Module):
                     self.mem_v_threshold = torch.where((self.mem_v > v_on) & (self.mem_v < v_off), torch.zeros_like(self.mem_v), torch.ones_like(self.mem_v))
                     self.mem_loss_time[self.mem_v_threshold == 0] += delta_t
                     self.mem_loss_time[self.mem_v_threshold == 1] = 0
-                    self.x -= self.x * delta_t * retention_loss_tau ** retention_loss_tau * retention_loss_beta * self.mem_loss_time ** (retention_loss_beta - 1)
+                    self.x -= self.x * delta_t * retention_loss_tau ** retention_loss_beta * retention_loss_beta * self.mem_loss_time ** (retention_loss_beta - 1)
                     self.x = torch.clamp(self.x, min = G_on, max = G_off)
+                    
+                    self.mem_x = G_off * self.mem_x + G_on * (1 - self.mem_x)
+                    self.mem_x -= self.mem_x * delta_t * retention_loss_tau ** retention_loss_beta * retention_loss_beta * self.mem_loss_time ** (retention_loss_beta - 1)
+                    self.mem_x = torch.clamp(self.mem_x, min = G_on, max = G_off)
+                    self.mem_x = (self.mem_x - G_on) * trans_ratio                    
                     
                 self.x = (self.x - G_on) * trans_ratio
                 
