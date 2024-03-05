ca84292c3e89c94f1cba4a9bf8efcf5fdca6a334
AACCAACT <frozenbitten@gmail.com>
2024-03-05 05:41:31 +0000

    update simbrain/__init__.py add memristor_fit
    Signed-off-by: AACCAACT <frozenbitten@gmail.com>

--- simbrain/__init__.py
+++ simbrain/__init__.py
@@ -1,3 +1,3 @@
-from simbrain import (mapping, memarray, mempower, memarea)
+from simbrain import (mapping, memarray, mempower, memarea, memristor_fit)
 from simbrain.mapping import Mapping
-__all__ = ["mapping", "memarray", "mempower", "Mapping", "memarea"]
+__all__ = ["mapping", "memarray", "mempower", "Mapping", "memarea", "memristor_fit"]
