fix area calculation problems

examples/CNN/vgg16_inference.py
@@ -38,11 +38,11 @@ parser.add_argument("--wire_width", type=int, default=200) # In practice, proces
 parser.add_argument("--CMOS_technode", type=int, default=32)
 parser.add_argument("--device_roadmap", type=str, default='HP') # HP: High Performance or LP: Low Power
 parser.add_argument("--temperature", type=int, default=300)
-parser.add_argument("--hardware_estimation", type=int, default=False)
+parser.add_argument("--hardware_estimation", type=int, default=True)
 args = parser.parse_args()
 
 # Sets up Gpu use
-os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, [0]))
+os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, [1]))
 seed = args.seed
 gpu = args.gpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -90,12 +90,12 @@ if sim_params['hardware_estimation']:
         if isinstance(layer, Mem_Conv2d):
             layer.crossbar.total_area_calculation()
             sim_area = layer.crossbar.sim_area
-            total_area += sim_area['mem_area']
+            total_area += sim_area['sim_total_area']
     if isinstance(net.classifier, Mem_Linear):
         layer = net.classifier
         layer.crossbar.total_area_calculation()
         sim_area = layer.crossbar.sim_area
-        total_area += sim_area['mem_area']
+        total_area += sim_area['sim_total_area']
     print("total_area=" + str(total_area))
 
 # Load Pre-trained Model

simbrain/memarray.py
@@ -87,8 +87,8 @@ class MemristorArray(torch.nn.Module):
         relax_ratio_col = self.memristor_info_dict[self.device_name]['relax_ratio_col'] # Leave space for adjacent memristors
         relax_ratio_row = self.memristor_info_dict[self.device_name]['relax_ratio_col'] # Leave space for adjacent memristors
         mem_size = self.memristor_info_dict[self.device_name]['mem_size']
-        length_row = shape[1] * relax_ratio_col * mem_size
-        length_col = shape[0] * relax_ratio_row * mem_size
+        self.length_row = shape[1] * relax_ratio_col * mem_size
+        self.length_col = shape[0] * relax_ratio_row * mem_size
         AR = self.tech_info_dict[str(self.wire_width)]['AR']
         Rho = self.tech_info_dict[str(self.wire_width)]['Rho']
         wire_resistance_unit_col = relax_ratio_col * mem_size * Rho / (AR * self.wire_width * self.wire_width * 1e-18)
@@ -99,8 +99,8 @@ class MemristorArray(torch.nn.Module):
 
         self.hardware_estimation = sim_params['hardware_estimation']
         if self.hardware_estimation:
-            self.power = Power(sim_params=sim_params, shape=self.shape, memristor_info_dict=self.memristor_info_dict, length_row=length_row, length_col=length_col)
-            self.area = Area(sim_params=sim_params, shape=self.shape, memristor_info_dict=self.memristor_info_dict, length_row=length_row, length_col=length_col)
+            self.power = Power(sim_params=sim_params, shape=self.shape, memristor_info_dict=self.memristor_info_dict, length_row=self.length_row, length_col=self.length_col)
+            self.area = Area(sim_params=sim_params, shape=self.shape, memristor_info_dict=self.memristor_info_dict, length_row=self.length_row, length_col=self.length_col)
 
 
     def set_batch_size(self, batch_size) -> None:

simbrain/periphpower.py
@@ -80,7 +80,7 @@ class DAC_Module_Power(torch.nn.Module):
         mem_size = self.memristor_info_dict[self.device_name]['mem_size']
         length_col = self.shape[0] * self.relax_ratio_col * mem_size
         if length_col < min_cell_height:
-            raise ValueError("[SwitchMatrix] Pass gate width is even much larger than the array width! You need to choose smaller technode, make relax_ratio_col or self.shape[0] larger.")
+            length_col = min_cell_height
         num_tg_pair_per_col = (int)(length_col / min_cell_height)
         num_col_tg_pair = (int)(math.ceil((float)(self.shape[0]) / num_tg_pair_per_col))
         num_tg_pair_per_col = (int)(math.ceil((float)(self.shape[0]) / num_col_tg_pair))
@@ -115,7 +115,7 @@ class DAC_Module_Power(torch.nn.Module):
         mem_size = self.memristor_info_dict[self.device_name]['mem_size']
         length_row = self.shape[1] * self.relax_ratio_row * mem_size
         if length_row < min_cell_width * 2:
-            raise ValueError("[SwitchMatrix] Pass gate length is even much larger than the array height! You need to choose smaller technode, make relax_ratio_row or self.shape[1] larger.")
+            length_row = min_cell_width * 2
         num_tg_pair_per_row = (int)(length_row / (min_cell_width * 2))
         num_row_tg_pair = (int)(math.ceil((float)(self.shape[1]) / num_tg_pair_per_row))
         num_tg_pair_per_row = (int)(math.ceil((float)(self.shape[1]) / num_row_tg_pair))