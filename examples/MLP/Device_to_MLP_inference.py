59a1408a7154985cd1eb3eec22ae1f519c745940
xjw <16110720017@fudan.edu.cn>
2024-07-28 13:15:29 +0800

    Fix MLP

--- examples/MLP/Device_to_MLP_inference.py
+++ examples/MLP/Device_to_MLP_inference.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 import time
+import sys
+
+sys.path.append('../')
 
 import torch
 import torch.nn.functional as F
@@ -10,6 +13,7 @@ from torch.autograd import Variable
 import dataset
 import mlp
 from module import *
+from Memristor_Modeling.full_fitting_flow import full_fitting
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int, default=0)
@@ -17,22 +21,7 @@ parser.add_argument("--gpu", dest="gpu", action="store_true", default='gpu')
 parser.add_argument("--rep", type=int, default=10)
 parser.add_argument("--batch_size", type=int, default=100)
 parser.add_argument('--data_root', default='data/', help='folder to save the model')
-parser.add_argument("--memristor_structure", type=str, default='crossbar') # trace, mimo or crossbar
-parser.add_argument("--memristor_device", type=str, default='ideal') # ideal, ferro, or hu
-parser.add_argument("--c2c_variation", type=bool, default=False)
-parser.add_argument("--d2d_variation", type=int, default=0) # 0: No d2d variation, 1: both, 2: Gon/Goff only, 3: nonlinearity only
-parser.add_argument("--stuck_at_fault", type=bool, default=False)
-parser.add_argument("--retention_loss", type=bool, default=False) # retention loss, 0: without it, 1: during pulse, 2: no pluse for a long time
-parser.add_argument("--aging_effect", type=int, default=0) # 0: No aging effect, 1: equation 1, 2: equation 2
-parser.add_argument("--input_bit", type=int, default=8)
-parser.add_argument("--ADC_precision", type=int, default=16)
-parser.add_argument("--ADC_setting", type=int, default=4)  # 2:two memristor crossbars use one ADC; 4:one memristor crossbar use one ADC
-parser.add_argument("--ADC_rounding_function", type=str, default='floor')  # floor or round
-parser.add_argument("--wire_width", type=int, default=200) # In practice, wire_width shall be set around 1/2 of the memristor size; Hu: 10um; Ferro:200nm;
-parser.add_argument("--CMOS_technode", type=int, default=45)
-parser.add_argument("--device_roadmap", type=str, default='HP') # HP: High Performance or LP: Low Power
-parser.add_argument("--temperature", type=int, default=300)
-parser.add_argument("--hardware_estimation", type=int, default=False)
+parser.add_argument("--memristor_structure", type=str, default='crossbar') # trace or crossbar
 args = parser.parse_args()
 
 # Sets up Gpu use
@@ -48,15 +37,8 @@ else:
         gpu = False
 print("Running on Device = ", device)
 
-# Mem device setup
-sim_params = {'device_structure': args.memristor_structure, 'device_name': args.memristor_device,
-              'c2c_variation': args.c2c_variation, 'd2d_variation': args.d2d_variation,
-              'stuck_at_fault': args.stuck_at_fault, 'retention_loss': args.retention_loss,
-              'aging_effect': args.aging_effect, 'wire_width': args.wire_width, 'input_bit': args.input_bit,
-              'CMOS_technode': args.CMOS_technode, 'ADC_precision': args.ADC_precision,
-              'ADC_setting': args.ADC_setting,'ADC_rounding_function': args.ADC_rounding_function,
-              'device_roadmap': args.device_roadmap, 'temperature': args.temperature,
-              'hardware_estimation': args.hardware_estimation}
+# %% Obtain memristor parameters
+sim_params = full_fitting(args.memristor_structure, None)
 
 t_begin = time.time()
 
@@ -88,7 +70,7 @@ for test_cnt in range(args.rep):
     for layer_name, layer in model.layers.items():
         if isinstance(layer, Mem_Linear):
             layer.mem_update()
-            if args.stuck_at_fault == True:
+            if sim_params['stuck_at_fault'] == True:
                 layer.crossbar.update_SAF_mask()
     end_time = time.time()
     exe_time = end_time - start_time
--- examples/MLP/Device_to_MLP_training.py
+++ examples/MLP/Device_to_MLP_training.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 import time
+import sys
+
+sys.path.append('../')
 
 from utee import misc
 import torch
@@ -11,6 +14,7 @@ from torch.autograd import Variable
 import dataset
 import mlp
 from module import *
+from Memristor_Modeling.full_fitting_flow import full_fitting
 
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--wd', type=float, default=0.0001, help='weight decay')
@@ -25,35 +29,13 @@ parser.add_argument('--logdir', default='log/default', help='folder to save to t
 parser.add_argument('--data_root', default='data/', help='folder to save the model')
 parser.add_argument('--decreasing_lr', default='80,120', help='decreasing strategy')
 parser.add_argument("--memristor_structure", type=str, default='crossbar') # trace, mimo or crossbar
-parser.add_argument("--memristor_device", type=str, default='ideal') # ideal, ferro, or hu
-parser.add_argument("--c2c_variation", type=bool, default=False)
-parser.add_argument("--d2d_variation", type=int, default=0) # 0: No d2d variation, 1: both, 2: Gon/Goff only, 3: nonlinearity only
-parser.add_argument("--stuck_at_fault", type=bool, default=False)
-parser.add_argument("--retention_loss", type=bool, default=False) # retention loss, 0: without it, 1: during pulse, 2: no pluse for a long time
-parser.add_argument("--aging_effect", type=int, default=0) # 0: No aging effect, 1: equation 1, 2: equation 2
-parser.add_argument("--input_bit", type=int, default=8)
-parser.add_argument("--ADC_precision", type=int, default=16)
-parser.add_argument("--ADC_setting", type=int, default=4)  # 2:two memristor crossbars use one ADC; 4:one memristor crossbar use one ADC
-parser.add_argument("--ADC_rounding_function", type=str, default='floor')  # floor or round
-parser.add_argument("--wire_width", type=int, default=200)  # In practice, wire_width shall be set around 1/2 of the memristor size; Hu: 10um; Ferro:200nm;
-parser.add_argument("--CMOS_technode", type=int, default=45)
-parser.add_argument("--device_roadmap", type=str, default='HP') # HP: High Performance or LP: Low Power
-parser.add_argument("--temperature", type=int, default=300)
-parser.add_argument("--hardware_estimation", type=int, default=False)
 args = parser.parse_args()
 args.logdir = os.path.join(os.path.dirname(__file__), args.logdir)
 misc.logger.init(args.logdir, 'train_log')
 print = misc.logger.info
 
-# Mem device setup
-sim_params = {'device_structure': args.memristor_structure, 'device_name': args.memristor_device,
-              'c2c_variation': args.c2c_variation, 'd2d_variation': args.d2d_variation,
-              'stuck_at_fault': args.stuck_at_fault, 'retention_loss': args.retention_loss,
-              'aging_effect': args.aging_effect, 'wire_width': args.wire_width, 'input_bit': args.input_bit,
-              'CMOS_technode': args.CMOS_technode, 'ADC_precision': args.ADC_precision,
-              'ADC_setting': args.ADC_setting, 'ADC_rounding_function': args.ADC_rounding_function,
-              'device_roadmap': args.device_roadmap, 'temperature': args.temperature,
-              'hardware_estimation': args.hardware_estimation}
+# %% Obtain memristor parameters
+sim_params = full_fitting(args.memristor_structure, None)
 
 # logger
 misc.ensure_dir(args.logdir)
@@ -79,7 +61,7 @@ print("Running on Device = " + str(device))
 train_loader, test_loader = dataset.get(batch_size=args.batch_size, data_root=args.data_root, num_workers=1)
 
 # %% Multiple test
-out_root = 'Accuracy_Results_MLP_train_nonlin_2.95.txt'
+out_root = 'Accuracy_Results_MLP_train.txt'
 multiple_test_no = 50
 for test_cnt in range(multiple_test_no):
     # model
@@ -100,7 +82,7 @@ for test_cnt in range(multiple_test_no):
     for layer_name, layer in model.layers.items():
         if isinstance(layer, Mem_Linear):
             layer.mem_update()
-            if args.stuck_at_fault == True:
+            if sim_params['stuck_at_fault'] == True:
                 layer.crossbar.update_SAF_mask()
 
     # optimizer
@@ -130,7 +112,7 @@ for test_cnt in range(multiple_test_no):
                 for layer_name, layer in model.layers.items():
                     if isinstance(layer, Mem_Linear):
                         layer.mem_update()
-                        if args.stuck_at_fault == True:
+                        if sim_params['stuck_at_fault'] == True:
                             layer.crossbar.update_SAF_mask()
 
                 if batch_idx % args.log_interval == 0 and batch_idx > 0:
